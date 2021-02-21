624551845786c59126637117a1bf3dcce1d38055
avivcaspi <avivcaspi@gmail.com>
2021-02-21 19:31:50 +0200

    fixed some bugs, now it runs much faster and find transformation automatically

--- src/court_detection.py
+++ src/court_detection.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from sympy import Point, Line
-from itertools import permutations, combinations
+from itertools import combinations
 
 
 def build_court_reference():
     line_width = 5
-    court = np.zeros((2408,1127), dtype=np.int)
+    court = np.zeros((2408, 1127), dtype=np.int)
     cv2.line(court, (10, 10), (1107, 10), 1, line_width)
     cv2.line(court, (10, 2388), (1107, 2388), 1, line_width)
     cv2.line(court, (10, 1199), (1107, 1199), 1, line_width)
@@ -18,9 +18,10 @@ def build_court_reference():
     cv2.line(court, (147, 10), (147, 2388), 1, line_width)
     cv2.line(court, (970, 10), (970, 2388), 1, line_width)
     cv2.line(court, (558, 559), (558, 1839), 1, line_width)
-    plt.imsave('court_reference.png', court, cmap='gray')
+    plt.imsave('court_configurations/court_reference.png', court, cmap='gray')
 
-#build_court_reference()
+
+# build_court_reference()
 
 
 class CourtDetector:
@@ -38,20 +39,33 @@ class CourtDetector:
                            8: [(970, 10), (1107, 10), (970, 2388), (1107, 2388)],
                            9: [(10, 10), (147, 10), (10, 2388), (147, 2388)],
                            10: [(147, 559), (558, 559), (147, 1839), (558, 1839)],
-                           11: [(558, 559), (970, 559), (558, 1839), (970, 1839)]}
-        self.court_reference = cv2.cvtColor(cv2.imread('court_reference.png'), cv2.COLOR_BGR2GRAY)
+                           11: [(558, 559), (970, 559), (558, 1839), (970, 1839)],
+                           12: [(147, 1839), (970, 1839), (147, 2388), (970, 2388)]}
+        # TODO add baseline, middle and some reference for lines in the court reference
+        self.court_reference = cv2.cvtColor(cv2.imread('court_configurations/court_reference.png'), cv2.COLOR_BGR2GRAY)
+        self.v_width = 0
+        self.v_height = 0
+        self.frame = None
+        self.gray = None
+        self.court_warp_matrix = None
+        self.game_warp_matrix = None
 
     def detect(self, frame):
+        self.frame = frame
+        self.v_height, self.v_width = frame.shape[:2]
         self.gray = self._threshold(frame)
 
-        gray = self._filter_pixels(self.gray)
+        filtered = self._filter_pixels(self.gray)
 
-        horizontal_lines, vertical_lines = self._detect_lines(gray)
+        horizontal_lines, vertical_lines = self._detect_lines(filtered)
 
-        self._find_homography(horizontal_lines, vertical_lines)
+        self.court_warp_matrix, self.game_warp_matrix = self._find_homography(horizontal_lines, vertical_lines)
+        '''game_warped = cv2.warpPerspective(self.frame, self.game_warp_matrix,
+                                          (self.court_reference.shape[1], self.court_reference.shape[0]))
+        cv2.imwrite('warped_game_.png', game_warped)
+        '''
 
     def _threshold(self, frame):
-        self.frame = frame
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray[gray > self.colour_threshold] = 255
         gray[gray < 255] = 0
@@ -77,95 +91,97 @@ class CourtDetector:
         minLineLength = 100
         maxLineGap = 20
         lines = cv2.HoughLinesP(gray, 1, np.pi / 180, 80, minLineLength=minLineLength, maxLineGap=maxLineGap)
+        lines = np.squeeze(lines)
+        display_lines_on_frame(self.frame.copy(), lines)
+
         horizontal, vertical = self._classify_lines(lines)
+        display_lines_on_frame(self.frame.copy(), horizontal, vertical)
 
         horizontal, vertical = self._merge_lines(horizontal, vertical)
-        frame = self.frame.copy()
-        cv2.line(frame, (int(len(gray[0]) * 4 / 7), 0), (int(len(gray[0]) * 4 / 7), 719), (255,255,0),2)
-        cv2.line(frame, (int(len(gray[0]) * 3 / 7), 0), (int(len(gray[0]) * 3 / 7), 719), (255, 255, 0), 2)
-        for line in horizontal:
-            for x1, y1, x2, y2 in line:
-                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.circle(frame, (x1, y1), 1, (255, 0, 0), 2)
-                cv2.circle(frame, (x2, y2), 1, (255, 0, 0), 2)
 
-        for line in vertical:
-            for x1, y1, x2, y2 in line:
-                cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                cv2.circle(frame, (x1, y1), 1, (255, 0, 0), 2)
-                cv2.circle(frame, (x2, y2), 1, (255, 0, 0), 2)
+        display_lines_on_frame(self.frame.copy(), horizontal, vertical)
 
-        ''' cv2.imshow('court', frame)
-        if cv2.waitKey(0) & 0xff == 27:
-            cv2.destroyAllWindows()'''
-        cv2.imwrite('houghlines5.jpg', frame)
         return horizontal, vertical
 
     def _classify_lines(self, lines):
+        # TODO find better way to classify lines
         horizontal = []
         vertical = []
-
+        highest_vertical_y = np.inf
+        lowest_vertical_y = 0
         for line in lines:
-            for x1, y1, x2, y2 in line:
-                dx = abs(x1 - x2)
-                dy = abs(y1 - y2)
-                if dx > dy:
-                    horizontal.append(line)
-                else:
-                    vertical.append(line)
-        return horizontal, vertical
+            x1, y1, x2, y2 = line
+            dx = abs(x1 - x2)
+            dy = abs(y1 - y2)
+            if dx > 2 * dy:
+                horizontal.append(line)
+            else:
+                vertical.append(line)
+                highest_vertical_y = min(highest_vertical_y, y1, y2)
+                lowest_vertical_y = max(lowest_vertical_y, y1, y2)
+        clean_horizontal = []
+        h = lowest_vertical_y - highest_vertical_y
+        lowest_vertical_y += h / 15
+        highest_vertical_y -= h * 2 / 15
+        for line in horizontal:
+            x1, y1, x2, y2 = line
+            if lowest_vertical_y > y1 > highest_vertical_y and lowest_vertical_y > y1 > highest_vertical_y:
+                clean_horizontal.append(line)
+        return clean_horizontal, vertical
 
     def _classify_vertical(self, vertical, width):
+        # TODO use vertical classification for improvement
         vertical_lines = []
         vertical_left = []
         vertical_right = []
         right_th = width * 4 / 7
         left_th = width * 3 / 7
         for line in vertical:
-            for x1, y1, x2, y2 in line:
-                if x1 < left_th or x2 < left_th:
-                    vertical_left.append(line)
-                elif x1 > right_th or x2 > right_th:
-                    vertical_right.append(line)
-                else:
-                    vertical_lines.append(line)
+            x1, y1, x2, y2 = line
+            if x1 < left_th or x2 < left_th:
+                vertical_left.append(line)
+            elif x1 > right_th or x2 > right_th:
+                vertical_right.append(line)
+            else:
+                vertical_lines.append(line)
         return vertical_lines, vertical_left, vertical_right
 
     def _merge_lines(self, horizontal_lines, vertical_lines):
-        horizontal_lines = sorted(horizontal_lines, key=lambda item: item[0, 0])
-        mask = [True]*len(horizontal_lines)
+        horizontal_lines = sorted(horizontal_lines, key=lambda item: item[0])
+        mask = [True] * len(horizontal_lines)
         new_horizontal_lines = []
         for i, line in enumerate(horizontal_lines):
             if mask[i]:
-                for j, s_line in enumerate(horizontal_lines[i+1:]):
-                    if mask[i+j+1]:
-                        x1, y1, x2, y2 = line[0]
-                        x3, y3, x4, y4 = s_line[0]
+                for j, s_line in enumerate(horizontal_lines[i + 1:]):
+                    if mask[i + j + 1]:
+                        x1, y1, x2, y2 = line
+                        x3, y3, x4, y4 = s_line
                         dy = abs(y3 - y2)
                         if dy < 10:
-                            line[0,2] = x4
-                            line[0,3] = y4
-                            mask[i+j+1] = False
+                            points = sorted([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], key=lambda x: x[0])
+                            line = np.array([*points[0], *points[-1]])
+                            mask[i + j + 1] = False
                 new_horizontal_lines.append(line)
 
-        vertical_lines = sorted(vertical_lines, key=lambda item: item[0,1])
-        #reference_horizontal_line = max(new_horizontal_lines, key=lambda item: item[0, 1])
-        xl,yl, xr,yr = (100, 300, 1000, 300)
+        vertical_lines = sorted(vertical_lines, key=lambda item: item[1])
+        xl, yl, xr, yr = (0, self.v_height * 6 / 7, self.v_width, self.v_height * 6 / 7)
         mask = [True] * len(vertical_lines)
         new_vertical_lines = []
         for i, line in enumerate(vertical_lines):
             if mask[i]:
-                for j, s_line in enumerate(vertical_lines[i+1:]):
-                    if mask[i+j+1]:
-                        x1, y1, x2, y2 = line[0]
-                        x3, y3, x4, y4 = s_line[0]
-                        xi, yi = line_intersection(((x1,y1), (x2,y2)), ((xl,yl), (xr,yr)))
+                for j, s_line in enumerate(vertical_lines[i + 1:]):
+                    if mask[i + j + 1]:
+                        x1, y1, x2, y2 = line
+                        x3, y3, x4, y4 = s_line
+                        xi, yi = line_intersection(((x1, y1), (x2, y2)), ((xl, yl), (xr, yr)))
                         xj, yj = line_intersection(((x3, y3), (x4, y4)), ((xl, yl), (xr, yr)))
+
                         dx = abs(xi - xj)
-                        if dx < 15:
-                            line[0,2] = x4
-                            line[0,3] = y4
-                            mask[i+j+1] = False
+                        if dx < 10:
+                            points = sorted([(x1, y1), (x2, y2), (x3, y3), (x4, y4)], key=lambda x: x[1])
+                            line = np.array([*points[0], *points[-1]])
+                            mask[i + j + 1] = False
+
                 new_vertical_lines.append(line)
         return new_horizontal_lines, new_vertical_lines
 
@@ -176,10 +192,8 @@ class CourtDetector:
         k = 0
         for horizontal_pair in list(combinations(horizontal_lines, 2)):
             for vertical_pair in list(combinations(vertical_lines, 2)):
-                h1 = horizontal_pair[0][0]
-                h2 = horizontal_pair[1][0]
-                v1 = vertical_pair[0][0]
-                v2 = vertical_pair[1][0]
+                h1, h2 = horizontal_pair
+                v1, v2 = vertical_pair
                 i1 = line_intersection((tuple(h1[:2]), tuple(h1[2:])), (tuple(v1[0:2]), tuple(v1[2:])))
                 i2 = line_intersection((tuple(h1[:2]), tuple(h1[2:])), (tuple(v2[0:2]), tuple(v2[2:])))
                 i3 = line_intersection((tuple(h2[:2]), tuple(h2[2:])), (tuple(v1[0:2]), tuple(v1[2:])))
@@ -198,7 +212,7 @@ class CourtDetector:
                         max_mat = matrix
                         max_inv_mat = inv_matrix
 
-                    k +=1
+                    k += 1
 
         frame = self.frame.copy()
         court = self.add_court_overlay(frame, max_mat, (255, 0, 0))
@@ -209,9 +223,7 @@ class CourtDetector:
         print(k)
         '''game_warped = cv2.warpPerspective(self.frame, in_mat,
                                           (self.court_reference.shape[1], self.court_reference.shape[0]))'''
-
-
-
+        return max_mat, max_inv_mat
 
     def _get_confi_score(self, matrix):
         court = cv2.warpPerspective(self.court_reference, matrix, self.frame.shape[1::-1])
@@ -240,7 +252,6 @@ def sort_intersection_points(intersections):
 
 
 def line_intersection(line1, line2):
-
     l1 = Line(line1[0], line1[1])
     l2 = Line(line2[0], line2[1])
 
@@ -248,11 +259,55 @@ def line_intersection(line1, line2):
     return intersection[0].coordinates
 
 
-#court_reference = cv2.cvtColor(cv2.imread('court_reference.png'), cv2.COLOR_BGR2GRAY)
-filename = '../images/img1.jpg'
-img = cv2.imread(filename)
-import time
-s = time.time()
-court_detector = CourtDetector()
-court_detector.detect(img)
-print(f'time = {time.time() - s}')
+def display_lines_on_frame(frame, horizontal=(), vertical=()):
+    cv2.line(frame, (int(len(frame[0]) * 4 / 7), 0), (int(len(frame[0]) * 4 / 7), 719), (255, 255, 0), 2)
+    cv2.line(frame, (int(len(frame[0]) * 3 / 7), 0), (int(len(frame[0]) * 3 / 7), 719), (255, 255, 0), 2)
+    for line in horizontal:
+        x1, y1, x2, y2 = line
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.circle(frame, (x1, y1), 1, (255, 0, 0), 2)
+        cv2.circle(frame, (x2, y2), 1, (255, 0, 0), 2)
+
+    for line in vertical:
+        x1, y1, x2, y2 = line
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        cv2.circle(frame, (x1, y1), 1, (255, 0, 0), 2)
+        cv2.circle(frame, (x2, y2), 1, (255, 0, 0), 2)
+
+    cv2.imshow('court', frame)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    cv2.imwrite('houghlines5.jpg', frame)
+    return frame
+
+
+def display_lines_and_points_on_frame(frame, lines=(), points=(), line_color=(0, 0, 255), point_color=(255, 0, 0)):
+    for line in lines:
+        x1, y1, x2, y2 = line
+        frame = cv2.line(frame, (x1, y1), (x2, y2), line_color, 2)
+    for p in points:
+        frame = cv2.circle(frame, p, 2, point_color, 2)
+
+    cv2.imshow('court', frame)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    return frame
+
+
+def save_all_court_configurations(court_conf, court_reference):
+    for i, conf in court_conf.items():
+        c = cv2.cvtColor(court_reference, cv2.COLOR_GRAY2BGR)
+        for p in conf:
+            c = cv2.circle(c, p, 15, (0, 0, 255), 30)
+        cv2.imwrite(f'court_configurations/court_conf_{i}.png', c)
+
+
+if __name__ == '__main__':
+    filename = '../images/img1.jpg'
+    img = cv2.imread(filename)
+    import time
+
+    s = time.time()
+    court_detector = CourtDetector()
+    court_detector.detect(img)
+    print(f'time = {time.time() - s}')
